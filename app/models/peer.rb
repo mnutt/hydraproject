4b482e0d30655359e4f8236526408ddbd865873b
shantibraford <shantibraford@ffe7e100-e63d-0410-a7fb-f560dcb15827>
2007-11-02 08:49:11 +0000

    infinite loop fix =)
    git-svn-id: http://hydraproject.googlecode.com/svn/trunk@11 ffe7e100-e63d-0410-a7fb-f560dcb15827

--- app/models/peer.rb
+++ app/models/peer.rb
@@ -2,15 +2,10 @@ class Peer < ActiveRecord::Base
   belongs_to :torrent
   
   before_create :ping
-  after_save "ping!"
+  before_save :ping
   
   def ping
     self.last_action_at = Time.now
   end
 
-  def ping!
-    self.last_action_at = Time.now
-    save!
-  end
-  
 end
