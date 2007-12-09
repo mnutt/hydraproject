de4d3c9ed174564ad7940b3602d8f117e5bd8870
shantibraford <shantibraford@ffe7e100-e63d-0410-a7fb-f560dcb15827>
2007-12-09 17:01:52 +0000

    require login on main index page
    git-svn-id: http://hydraproject.googlecode.com/svn/trunk@81 ffe7e100-e63d-0410-a7fb-f560dcb15827

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,16 @@
 class HomeController < ApplicationController
-  
+  before_filter :check_logged_in
+
   def index
     @active = Torrent.find_by_sql("SELECT torrents.*, (torrents.leechers + torrents.seeders) as active FROM torrents ORDER BY active DESC LIMIT 10")
     @best_seeded = Torrent.find(:all, :order => "seeders DESC", :limit => 100)
     @most_snatched = Torrent.find(:all, :conditions => ["seeders > ? AND times_completed > ?", 0, 0], :order => "times_completed DESC", :limit => 100)
     Torrent.clear_user_ids
   end
-
+  
+  def check_logged_in
+    flash[:notice] = "#{C[:app_name]} is a private tracker.  Please <a href=\"/account/signup\">Signup</a> or <a href=\"/account/login\">Login</a> to continue."
+    redirect_to :controller => :account, :action => :signup
+  end
+  
 end
