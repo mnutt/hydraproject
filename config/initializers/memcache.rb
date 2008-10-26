beeb0b5e6c654a540af216aeeb81151ed25b4410
Michael Nutt <michael@nuttnet.net>
2008-10-26 16:30:57 -0400

    Don't start memcached if running in test env

--- config/initializers/memcache.rb
+++ config/initializers/memcache.rb
@@ -1,9 +1,11 @@
-CACHE = MemCache.new "localhost:#{C[:num_memcached_port]}", :namespace => 'hydra'
+unless ENV['RAILS_ENV'] == "test"
+  CACHE = MemCache.new "localhost:#{C[:num_memcached_port]}", :namespace => 'hydra'
 
-# Ensure memcached is running
-begin
-  CACHE.get('foo')
-rescue MemCache::MemCacheError
-  puts "\nStarting memcached...\n"
-  system("memcached -d -m #{C[:num_memcached_memory]} -p #{C[:num_memcached_port]}")
+  # Ensure memcached is running
+  begin
+    CACHE.get('foo')
+  rescue MemCache::MemCacheError
+    puts "\nStarting memcached...\n"
+    system("memcached -d -m #{C[:num_memcached_memory]} -p #{C[:num_memcached_port]}")
+  end
 end
