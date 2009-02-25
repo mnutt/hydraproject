325c0f061009f7a2f9152d4e2c24ee03a6f8fcbc
Michael Nutt <michael@nuttnet.net>
2009-02-25 16:56:08 -0500

    Rails adapter thing changed, I guess?

--- db/migrate/009_full_text_search.rb
+++ db/migrate/009_full_text_search.rb
@@ -1,15 +1,15 @@
 class FullTextSearch < ActiveRecord::Migration
   def self.up
-    if ActiveRecord::Base.configurations[ENV['RAILS_ENV']]["adapter"] == "mysql"
+    #if ActiveRecord::Base.configurations[ENV['RAILS_ENV']]["adapter"] == "mysql"
       execute 'ALTER TABLE torrents ENGINE = MyISAM'
       execute 'CREATE FULLTEXT INDEX ft_idx_torrents ON torrents(name,filename)'
-    end
+    #end
   end
 
   def self.down
-    if ActiveRecord::Base.configurations[ENV['RAILS_ENV']]["adapter"] == "mysql"
+    #if ActiveRecord::Base.configurations[ENV['RAILS_ENV']]["adapter"] == "mysql"
       execute 'ALTER TABLE torrents DROP INDEX ft_idx_torrents'
       execute 'ALTER TABLE torrents ENGINE = InnoDB'
-    end
+    #end
   end
 end
